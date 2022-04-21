c0956dbc71e1c13ff202c60887dea304207e0fba
Ale <72107717+Ale-eth@users.noreply.github.com>
2022-04-21 19:01:26 -0300

    finalizado

--- src/Recuperatorio/recuperatorio.java
+++ src/Recuperatorio/recuperatorio.java
@@ -1,4 +1,3 @@
-package Recuperatorio;
 /*
         Se tiene una matriz de caracteres de tamanio 3*20 de secuencias de caracteres vocales minusculas (por cada fila) separadas por espacios.
         La matriz esta precargada (no implementar el metodo de carga), y ademas cada fila empieza y termina con uno o mas separadores espacios.
@@ -16,66 +15,174 @@ public class recuperatorio {
                                 {'s', 'c', 'x', ' ', 'f', ' ', 'i', 'f', 'c', 'e', 'h', 'n', ' ', 'g', 's', 'q', ' ', 'k', 'l', 'i'}};
 
     public static void main(String[] args) {
-        recorrer_filas();
+        imprimir();
+        resolver_ejercicio();
+        imprimir();
 
     }
 
-        public static void recorrer_filas(){
-            for (int i = 0; i < MAXFILAS ; i++) {
-                detectar_secuencia(i);
-                System.out.println("");
+    public static void imprimir(){
+        for (int i = 0; i < MAXFILAS; i++){
+            System.out.println("");
+            for (int j = 0; j < MAXCOL; j++){
+                System.out.print(matchar[i][j] + ", ");
             }
         }
-            public static void detectar_secuencia(int fila){
-                int inicioSec_columna;
-                int finSec_columna;
+        System.out.println("");
+    }
+    public static void resolver_ejercicio(){
+        for (int i = 0; i < MAXFILAS ; i++) {
+            detectar_secuencia(i);
+            System.out.println("");
+        }
+    }
+    public static void detectar_secuencia(int fila){
+        int inicioSec_columna;
+        int finSec_columna;
 
-                for (int i = 0; i < MAXCOL-1; i++) {
-                    inicioSec_columna = detectar_inicio(fila, i);
-                    finSec_columna = detectar_fin(fila, inicioSec_columna);
-                    i = finSec_columna;
-                    System.out.print(inicioSec_columna +" "+ (finSec_columna)+ " --- ");
-                    cambiar(inicioSec_columna, finSec_columna, fila);
+        System.out.println("");
+
+        for (int i = 0; i < MAXCOL-1; i++) {
+            inicioSec_columna = detectar_inicio(fila, i);
+            finSec_columna = detectar_fin(fila, inicioSec_columna);
+            i = finSec_columna;
+            System.out.print(inicioSec_columna +" "+ (finSec_columna)+ " --- ");
+            cambiar(inicioSec_columna, finSec_columna, fila);
+        }
+
+    }
+    public static int detectar_inicio(int fila, int columna){
+        while(columna <= MAXCOL-1 && matchar[fila][columna] == ' '){
+            columna++;
+        }
+        return columna;
+    }
+    public static int detectar_fin(int fila, int inicio){
+        while(inicio <= MAXCOL-1 && matchar[fila][inicio] != ' '){
+            inicio++;
+        }
+        return inicio - 1;
+    }
+
+    public static void cambiar(int inicio, int fin,int fila){
+        if(secuenciaVocales(inicio, fin, fila) || secuenciaConsonantes(inicio, fin, fila)){
+            matchar[fila][inicio] = matchar[fila][fin];
+        }
+    }
+    public static boolean secuenciaVocales(int inicio, int fin, int fila){
+        boolean todasVocales = false;
+
+        boolean a = false;
+        boolean e = false;
+        boolean i = false;
+        boolean o = false;
+        boolean u = false;
+
+        while(inicio <= fin){
+            if(matchar[fila][inicio] == 'a' || matchar[fila][inicio] == 'e' || matchar[fila][inicio] == 'i' || matchar[fila][inicio] == 'o' || matchar[fila][inicio] == 'u'){
+                switch(matchar[fila][inicio]){
+                    case 'a' -> a = true;
+                    case 'e' -> e = true;
+                    case 'i' -> i = true;
+                    case 'o' -> o = true;
+                    case 'u' -> u = true;
                 }
-
+                inicio++;
+            }else{
+                todasVocales = false;
+                break;
             }
-                public static int detectar_inicio(int fila, int columna){
-                    while(columna <= MAXCOL-1 && matchar[fila][columna] == ' '){
-                        columna++;
-                    }
-                    return columna;
+        }
+
+        if (a && e && i && o && u){
+            todasVocales = true;
+        }
+        return todasVocales;
+    }
+
+    public static boolean secuenciaConsonantes(int inicio, int fin, int fila) {
+
+        boolean todasConsonantes = false;
+
+        boolean b = false;
+        boolean c = false;
+        boolean d = false;
+        boolean f = false;
+        boolean g = false;
+        boolean h = false;
+        boolean j = false;
+        boolean k = false;
+        boolean l = false;
+        boolean m = false;
+        boolean n = false;
+        boolean p = false;
+        boolean q = false;
+        boolean r = false;
+        boolean s = false;
+        boolean t = false;
+        boolean v = false;
+        boolean w = false;
+        boolean x = false;
+        boolean y = false;
+        boolean z = false;
+
+        while (inicio <= fin) {
+            if (
+                    matchar[fila][inicio] == 'b' ||
+                            matchar[fila][inicio] == 'c' ||
+                            matchar[fila][inicio] == 'd' ||
+                            matchar[fila][inicio] == 'f' ||
+                            matchar[fila][inicio] == 'g' ||
+                            matchar[fila][inicio] == 'h' ||
+                            matchar[fila][inicio] == 'j' ||
+                            matchar[fila][inicio] == 'k' ||
+                            matchar[fila][inicio] == 'l' ||
+                            matchar[fila][inicio] == 'm' ||
+                            matchar[fila][inicio] == 'n' ||
+                            matchar[fila][inicio] == 'p' ||
+                            matchar[fila][inicio] == 'q' ||
+                            matchar[fila][inicio] == 'r' ||
+                            matchar[fila][inicio] == 's' ||
+                            matchar[fila][inicio] == 't' ||
+                            matchar[fila][inicio] == 'v' ||
+                            matchar[fila][inicio] == 'w' ||
+                            matchar[fila][inicio] == 'x' ||
+                            matchar[fila][inicio] == 'y' ||
+                            matchar[fila][inicio] == 'z'
+            ) {
+                switch (matchar[fila][inicio]) {
+                    case 'b' -> b = true;
+                    case 'c' -> c = true;
+                    case 'd' -> d = true;
+                    case 'f' -> f = true;
+                    case 'g' -> g = true;
+                    case 'h' -> h = true;
+                    case 'j' -> j = true;
+                    case 'k' -> k = true;
+                    case 'l' -> l = true;
+                    case 'm' -> m = true;
+                    case 'n' -> n = true;
+                    case 'p' -> p = true;
+                    case 'q' -> q = true;
+                    case 'r' -> r = true;
+                    case 's' -> s = true;
+                    case 't' -> t = true;
+                    case 'v' -> v = true;
+                    case 'w' -> w = true;
+                    case 'x' -> x = true;
+                    case 'y' -> y = true;
+                    case 'z' -> z = true;
                 }
-                public static int detectar_fin(int fila, int inicio){
-                    while(inicio <= MAXCOL-1 && matchar[fila][inicio] != ' '){
-                        inicio++;
-                    }
-                    return inicio - 1;
-                }
+                inicio++;
+            } else {
+                todasConsonantes = false;
+                break;
+            }
+        }
 
-                public static void cambiar(int inicio, int fin,int fila){
-                    if(secuenciaVocales(inicio, fin, fila) || secuenciaConsonantes(inicio, fin)){
-
-                    }
-                }
-                    public static boolean secuenciaVocales(int inicio, int fin, int fila){
-                    boolean todasVocales = false;
-                    boolean todasConsonantes = false;
-                        while(inicio <= fin){
-                            if(matchar[fila][inicio] == 'a' || matchar[fila][inicio] == 'e' || matchar[fila][inicio] == 'i' || matchar[fila][inicio] == 'o' || matchar[fila][inicio] == 'u'){
-                                todasVocales = true;
-                                inicio++;
-                            }else{
-                                todasVocales = false;
-                                todasConsonantes = true;
-                                break;
-                            }
-                        }
-                    return todasVocales;
-                    }
-
-                    public static boolean secuenciaConsonantes(int inicio, int fin){
-
-                    }
+        if (b && c && d && f && g && h && j && k && l && m && n && p && q && r && s && t && v && w && y && z){
+            todasConsonantes = true;
+        }
+        return todasConsonantes;
+    }
 }
-
-
